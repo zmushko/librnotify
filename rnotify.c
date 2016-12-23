067fb29f3c71ec39bac346c22f93866cf097d03d
Andrey Zmushko <andrey.zmushko@itspartner.net>
2016-12-23 16:59:09 +0300

    get rid from one include

--- rnotify.c
+++ rnotify.c
@@ -8,9 +8,38 @@
 #include <stddef.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/inotify.h>
+#include <regex.h>
 
-#include "rnotify_internal.h"
 #include "rnotify.h"
+
+struct chainEvent
+{
+        struct chainEvent* prev;
+        struct inotify_event* e;
+        struct chainEvent* next;
+};
+
+struct Cookie {
+        struct Cookie* prev;
+        uint32_t cookie;
+        char* path;
+        char* name;
+        struct Cookie* next;
+};
+
+struct _rnotify {
+        int fd;
+        unsigned int size_w;
+        char** w;
+        long max_name;
+        unsigned long max_queued_events;
+        uint32_t mask;
+        regex_t* exclude;
+        struct chainEvent* head;
+        struct chainEvent* tail;
+        struct Cookie* cookies;
+};
 
 #define PATH_MAX_QUEUED_EVENTS	"/proc/sys/fs/inotify/max_queued_events"
 
--- rnotify.h
+++ rnotify.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <sys/inotify.h>
 
-typedef struct _notify Notify;
+typedef struct _rnotify Notify;
 
 Notify*	initNotify(char** path, const uint32_t mask, const char* exclude);
 int	waitNotify(Notify* ntf, char** const path, uint32_t* mask, const int timeout, uint32_t* cookie);
